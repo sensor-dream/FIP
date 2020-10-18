be54f97292830561653c208493a6d338204ce876
sensor-dream <sensor-dream@sensor-dream.ru>
2020-10-18 10:49:16 +0700

    mailto: sensor-dream@sensor-dream.ru commiter: auto-commit

---
+++ tools/setperm.bash
@@ -0,0 +1,24 @@
+#!/bin/env bash
+
+export setperm_user="$(id -un)"
+export setperm_group="apache"
+
+[[ -f "README.md" ]] || cd ../
+
+multiple_cmd() {
+  if [[ "$0" != "./.git"* && "$0" != "./tools" && ! -L "$0" ]]; then
+    echo "Find: $0"
+    if [[ -f "$0" ]]; then
+      sudo chmod 0664 "$0"
+    else
+      sudo chmod 0775 "$0"
+    fi
+    sudo chown ${setperm_user}.${setperm_group} "$0"
+  fi
+}
+
+export -f multiple_cmd
+
+#find . -exec bash -c 'multiple_cmd "$0"' {} \;
+
+find . -type d -exec bash -c 'multiple_cmd "$0"' {} \;
